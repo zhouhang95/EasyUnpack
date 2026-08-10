31d254e5b2bf4c4944d9165647e3a100dd0bd1e1
zhouhang95 <zhouhang95@foxmail.com>
2026-08-10 16:39:53 +0800

    Initial Commit

---
+++ EasyUnpack/EasyUnpackApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyUnpackApp.swift
+//  EasyUnpack
+//
+//  Created by ZhouHang on 2026/8/10.
+//
+
+import SwiftUI
+
+@main
+struct EasyUnpackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
